0426901b8cea47fd851468ef073f82e7c9bda150
Carl Guan <xicong.guan@gmail.com>
2024-07-11 10:39:11 -0400

    add notifications for if credentials already exist and successfully submitting new credentials

--- popup/popup_script.js
+++ popup/popup_script.js
@@ -1,6 +1,27 @@
 // TODO: inform the user if they already have data stored
 // show different popup content
 
+const makeVisible = "visibility: visible; display: inline;";
+const makeInvisible = "visibility: hidden; display: none;";
+
+const providedMessage = document.getElementById("provided-message");
+const successMessage = document.getElementById("success-message");
+
+function checkExistingCredentials() {
+  try {
+    browser.storage.local
+      .get(["baseId", "tableId", "patToken"])
+      .then((item) => {
+        const { baseId, tableId, patToken } = item;
+        if (baseId && tableId && patToken) {
+          providedMessage.setAttribute("style", makeVisible);
+        }
+      });
+  } catch (error) {
+    console.log(`error occurred while fetching airtable credentials: ${error}`);
+  }
+}
+
 function setAirTableData() {
   const submitButton = document.getElementById("submit-data");
   console.log(submitButton);
@@ -12,7 +33,13 @@ function setAirTableData() {
     browser.runtime.sendMessage({
       header: "reloadCredentials",
     });
+    providedMessage.setAttribute("style", makeInvisible);
+    successMessage.setAttribute("style", makeVisible);
   });
 }
 
+// if credentials already exist, inform user with message
+checkExistingCredentials();
+
+// add listener for Submit button
 setAirTableData();
